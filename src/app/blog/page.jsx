82c66ca48b826190df754d16930ae5fe95d873a6
Tsotneshonia1 <151642509+Tsotneshonia1@users.noreply.github.com>
2024-12-03 02:56:14 +0400

    Adding_Portfoilio_Blog

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,11 +1,88 @@
-import React from 'react'
-import styles from './page.module.css';
-
+import React from "react";
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Blog = () => {
   return (
-    <div className={styles.container}>Blog</div>
-  )
-}
+    <div className={styles.mainContainer}>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/11399666/pexels-photo-11399666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.Image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h2 className={styles.title}>Test</h2>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/11399666/pexels-photo-11399666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.Image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h2 className={styles.title}>Test</h2>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/11399666/pexels-photo-11399666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.Image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h2 className={styles.title}>Test</h2>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/11399666/pexels-photo-11399666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.Image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h2 className={styles.title}>Test</h2>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="/blog/testId" className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/11399666/pexels-photo-11399666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.Image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h2 className={styles.title}>Test</h2>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
+  );
+};
 
-export default Blog
+export default Blog;
