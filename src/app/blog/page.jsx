bfee452dd27fa64f713ba6203555e78749a187b9
Tsotneshonia1 <151642509+Tsotneshonia1@users.noreply.github.com>
2024-12-06 01:35:17 +0400

    adding_only_blog_Title_fetch

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -3,84 +3,40 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-const Blog = () => {
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+const Blog = async () => {
+  const data = await getData();
+
   return (
     <div className={styles.mainContainer}>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/11399666/pexels-photo-11399666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.Image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h2 className={styles.title}>Test</h2>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/11399666/pexels-photo-11399666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.Image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h2 className={styles.title}>Test</h2>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/11399666/pexels-photo-11399666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.Image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h2 className={styles.title}>Test</h2>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/11399666/pexels-photo-11399666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.Image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h2 className={styles.title}>Test</h2>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
-      <Link href="/blog/testId" className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src="https://images.pexels.com/photos/11399666/pexels-photo-11399666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={400}
-            height={250}
-            className={styles.Image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h2 className={styles.title}>Test</h2>
-          <p className={styles.desc}>Desc</p>
-        </div>
-      </Link>
+      {data.map((item) => (
+        <Link href="/blog/testId" className={styles.container} key={item.id}>
+          <div className={styles.imageContainer}>
+            <Image
+              src="https://images.pexels.com/photos/11399666/pexels-photo-11399666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={400}
+              height={250}
+              className={styles.Image}
+            />
+          </div>
+          <div className={styles.content}>
+            <h2 className={styles.title}>{item.Title}</h2>
+            <p className={styles.desc}>Desc</p>
+          </div>
+        </Link>
+      ))}
     </div>
   );
 };
